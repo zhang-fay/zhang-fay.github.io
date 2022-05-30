393176a86cc5143f7417e18b1fc126e6cd7b86fa
zhang-fay <zhangfay@student.utwente.nl>
2022-05-30 12:09:37 +0200

    new changes
    progress

--- main.py
+++ main.py
@@ -214,6 +214,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+server = app.server
 
 # Layout section: BootStrap
 # ----------------------------------------------------
